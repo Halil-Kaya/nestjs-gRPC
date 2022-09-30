98a71d0cb0568c111d10c5d8fdbccfbeaf683e00
Halil Kaya <halilkaya@teknasyon.com>
2022-09-30 22:53:03 +0300

    feat(filters): Add filters lib.

---
+++ libs/filters/src/all.exceptions.filter.ts
@@ -0,0 +1,38 @@
+import {
+  ExceptionFilter,
+  ArgumentsHost,
+  Catch,
+  Logger
+} from "@nestjs/common";
+import { GeneralServerErrorException } from "exceptions/exceptions";
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  private logger = new Logger(AllExceptionsFilter.name);
+
+  catch(exception: any, host: ArgumentsHost): any {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+    if (!exception.isCustomError) {
+      exception = new GeneralServerErrorException();
+    }
+    if (exception.metadata) {
+      exception = JSON.parse(exception.metadata.get("customError")[0]);
+    }
+    this.logger.error(
+      `[ERROR:${exception.errorCode}] ${exception.message.toUpperCase()}`
+    );
+    response.status(500).json({
+      meta: {
+        headers: request.headers,
+        params: request.params,
+        status: request.status,
+        errorCode: exception.errorCode,
+        errorMessage: exception.message,
+        timestamp: new Date()
+      },
+      result: exception
+    });
+  }
+}
---
+++ libs/filters/src/grpc-all.exceptions.filter.ts
@@ -0,0 +1,26 @@
+import {
+  ArgumentsHost,
+  Catch,
+  Logger
+} from "@nestjs/common";
+import { BaseRpcExceptionFilter } from "@nestjs/microservices";
+import { throwError } from "rxjs";
+import { Metadata } from "@grpc/grpc-js";
+import { GeneralServerErrorException } from "exceptions/exceptions";
+
+@Catch()
+export class GrpcAllExceptionsFilter extends BaseRpcExceptionFilter {
+  private logger = new Logger(GrpcAllExceptionsFilter.name);
+
+  catch(exception: any, host: ArgumentsHost) {
+    if (!exception.isCustomError) {
+      exception = new GeneralServerErrorException();
+    }
+    this.logger.error(`[ERROR:${exception.errorCode}] ${exception.message.toUpperCase()}`);
+    const errorMetadata = new Metadata();
+    errorMetadata.add("customError", JSON.stringify(exception));
+    return throwError(() => {
+      metadata : exception;
+    });
+  }
+}
