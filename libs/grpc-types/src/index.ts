0db40f7bf7c0136ab62473ff73e3fdb6fc300ff9
Halil Kaya <halilkaya@teknasyon.com>
2022-10-22 16:27:41 +0300

    feat(grpc-types): Move proto files to protos folder.

--- libs/grpc-types/src/index.ts
+++ libs/grpc-types/src/index.ts
@@ -1,5 +1,5 @@
-import * as UserProto from "./user";
-import * as AuthProto from "./auth";
+import * as UserProto from "./protos/user";
+import * as AuthProto from "./protos/auth";
 import * as GrpcClients from "./clients";
 
 export {
