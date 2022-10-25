e238e087cb100eaf21a4a76d3aa17018f3206744
Halil Kaya <halilkaya@teknasyon.com>
2022-10-25 21:24:52 +0300

    feat(interceptors-lib): Add log to transform interceptor.

--- libs/interceptors/src/transform.interceptor.ts
+++ libs/interceptors/src/transform.interceptor.ts
@@ -1,4 +1,4 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from "@nestjs/common";
 import { map } from "rxjs/operators";
 import { Observable } from "rxjs";
 
@@ -10,32 +10,35 @@ export interface MetaInterface {
   timestamp: Date;
 }
 
-export interface Response {
+export interface Response<T> {
   meta: MetaInterface;
-  result: any;
+  result: T;
 }
 
 @Injectable()
 export class TransformInterceptor
   implements NestInterceptor {
-
+  private logger = new Logger("HTTP");
 
   intercept(
     context: ExecutionContext,
     next: CallHandler
-  ): Observable<Response> {
+  ): Observable<Response<any>> {
+    const { method, url, body, headers, params, status } = context.switchToHttp().getRequest();
+    this.logger.log(`REQ [${method} ${url}]:-> ${JSON.stringify(body)}`);
     return next.handle().pipe(
       map((data) => {
-        const req = context.switchToHttp().getRequest();
-        return {
+        const res = {
           meta: {
-            headers: req.headers,
-            params: req.params,
-            status: req.status,
+            headers: headers,
+            params: params,
+            status: status,
             timestamp: new Date()
           },
           result: data
         };
+        this.logger.log(`RES [${method} ${url}] :-> ${JSON.stringify(res)}`);
+        return res;
       })
     );
   }
