abfedcb83853aa32f4ebbf3f7ac44abb8d2c6a52
Halil Kaya <halilkaya@teknasyon.com>
2022-11-01 17:31:52 +0300

    build(docker): Add port to user-service.

--- libs/grpc-types/src/config.ts
+++ libs/grpc-types/src/config.ts
@@ -1,35 +1,35 @@
 export const AuthConfig = {
-  hostname: 'auth-service',
+  hostname: "auth-service",
   port: 5055,
-  path: './../../auth.proto',
+  path: "./../../auth.proto",
   loader: {
     keepCase: true,
     longs: Number,
     enums: String,
-    arrays: true,
-  },
+    arrays: true
+  }
 };
 
 export const UserConfig = {
   hostname: "user-service",
   port: 5056,
-  path: './../../user.proto',
+  path: "./../../user.proto",
   loader: {
     keepCase: true,
     longs: Number,
     enums: String,
-    arrays: true,
-  },
+    arrays: true
+  }
 };
 
 export const TodoConfig = {
-  hostname: 'todo-service',
+  hostname: "todo-service",
   port: 5058,
-  path: './../../todo.proto',
+  path: "./../../todo.proto",
   loader: {
     keepCase: true,
     longs: Number,
     enums: String,
-    arrays: true,
-  },
+    arrays: true
+  }
 };
